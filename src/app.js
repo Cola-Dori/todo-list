67c5be8a849128d82c58c909eec02b1ff34aff85
yunho <kimyouknow@naver.com>
2022-04-13 14:33:25 +0900

    Refactor: app.js에 init함수 추가
    DOMContentLoaded이벤트로 init함수 실행

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import { $, closeModal, eventDelegate } from './utils/utils';
 import '../styles/main.scss';
 import TodoColumns from './Views/TodoColumns';
-import './Views/Modal';
 import store from './store';
 
-const todoColumns = new TodoColumns($('.todo-columns'));
+function init() {
+  const todoColumns = new TodoColumns($('.todo-columns'));
+  eventDelegate({
+    target: $('.modal'),
+    eventName: 'click',
+    selector: '.modal .cancel-button',
+    handler: closeModal,
+  });
 
-eventDelegate({
-  target: $('.modal'),
-  eventName: 'click',
-  selector: '.modal .cancel-button',
-  handler: closeModal,
-});
+  todoColumns.init(store.getAllColumns());
+}
 
-todoColumns.init(store.getAllColumns());
+window.addEventListener('DOMContentLoaded', init);
